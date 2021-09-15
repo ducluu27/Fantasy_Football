6c5d38a4fe70c2891cab6a92dbbcba4c2cc7b32c
ducluu27 <67710414+ducluu27@users.noreply.github.com>
2021-09-14 23:11:33 -0500

    update with new web scraper and update with sql tables

--- SQL Analysis.sql
+++ SQL Analysis.sql
@@ -1,3 +1,4 @@
+-- QB Analysis
 select [Player], [Tm], [Year], [FantPos], [PosRank], [Cmp], [Att] ,[TD], [PPR], CAST([TD] as decimal)/ [Cmp] as cmp_td_per
 from [dbo].[player_stats]
 where FantPos = 'QB' 
@@ -5,3 +6,19 @@ where FantPos = 'QB'
 and year = '2020'
 and PosRank <= 20
 order by 10 desc
+
+-- Creating Stored Procudures to link every team to weekly scraped data
+select  Week_1.*, bk.Position 
+from BK_Team as bk
+inner join Week_1
+on bk.[Player] = [dbo].[Week_1].[Player]
+
+-- Create a View for teams
+create view Canine_Cup as 
+select  Week_1.*, cc.Position 
+from Canine_Cup_Team as cc
+inner join Week_1
+on cc.[Player] = [dbo].[Week_1].[Player]
+
+select *
+from Canine_Cup
